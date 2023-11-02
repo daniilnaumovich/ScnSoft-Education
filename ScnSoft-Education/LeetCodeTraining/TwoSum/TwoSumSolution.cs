84437832d5d01ee86b3cc71c6d58a326ed72aaf3
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-02 14:46:57 +0300

    #1 The solution for two sum problem (No Dictionary used yet, test purposes)

---
+++ ScnSoft-Education/LeetCodeTraining/TwoSum/TwoSumSolution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTraining.TwoSum
+{
+    //https://leetcode.com/problems/two-sum/
+    public static class TwoSumSolution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[2] { i, j };
+                    }
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}
