c44f44f3f3acb675670310d873cb824b6f2660a1
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-04 00:08:06 +0300

    Addressed issues mentioned in comments.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/TwoSumTests.cs
@@ -0,0 +1,43 @@
+﻿using LeetCodeTraining.TwoSum;
+
+namespace LeetCodeTraining.Tests
+{
+    public class TwoSumSolutionTests
+    {
+        [Theory]
+        [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+        [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        public void TwoSum_ArrayWithMultipleNums(int[] nums, int target, int[] expectedResult)
+        {
+            //Act
+            var result = TwoSumSolution.TwoSum(nums, target);
+            Array.Sort(result);
+
+            //Assert
+            Assert.Equal(expectedResult.Length, result.Length);       
+            for (int i = 0; i < expectedResult.Length; i++)
+            {
+                Assert.Equal(expectedResult[i], result[i]);
+            }
+        }
+
+        [Theory]
+        [InlineData(new int[] { 3, 3 }, 6, new int[] { 0, 1 })]
+        [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        public void TwoSum_ArrayWithMultipleNumsDictionary(int[] nums, int target, int[] expectedResult)
+        {
+            //Act
+            var result = TwoSumSolution.TwoSumDictionary(nums, target);
+            Array.Sort(result);
+
+            //Assert
+            Assert.Equal(expectedResult.Length, result.Length);         
+            for(int i = 0; i< expectedResult.Length; i++)
+            {
+                Assert.Equal(expectedResult[i], result[i]);
+            }
+        }
+    }
+}
--- ScnSoft-Education/LeetCodeTraining/TwoSum/TwoSumSolution.cs
+++ ScnSoft-Education/LeetCodeTraining/TwoSum/TwoSumSolution.cs
@@ -1,14 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCodeTraining.TwoSum
+﻿namespace LeetCodeTraining.TwoSum
 {
     //https://leetcode.com/problems/two-sum/
     public static class TwoSumSolution
     {
+        public static int[] TwoSumDictionary(int[] nums, int target)
+        {
+            Dictionary<int, int> inputMap = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!inputMap.ContainsKey(nums[i]))
+                {
+                    inputMap.Add(nums[i], i);
+                }
+                //the cases when we have duplicate values in input array are:
+                //1) the sum of duplicated values is the target value - return their indeces
+                //2) both of the duplicated values are not components of target value because of discrepancy (only one answer)
+                else if (nums[i] * 2 == target)
+                {
+                    return new int[2] { i, inputMap[nums[i]] };
+                }
+
+                continue;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int correspondingValue = target - nums[i];
+
+                if (inputMap.ContainsKey(correspondingValue) && inputMap[correspondingValue] != i)
+                {
+                    return new int[2] { i, inputMap[correspondingValue] };
+                }
+            }
+
+            return new int[] { };
+        }
+
         public static int[] TwoSum(int[] nums, int target)
         {
             for (int i = 0; i < nums.Length; i++)
