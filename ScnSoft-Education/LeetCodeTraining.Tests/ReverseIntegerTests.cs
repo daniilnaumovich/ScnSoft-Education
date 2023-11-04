b55f7c57987b431ec2e913a948b0e4f7cd6c5b77
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-04 21:15:15 +0300

    #7. The solution for Reverse integer problem.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/ReverseIntegerTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeTraining.ReverseInteger;
+
+namespace LeetCodeTraining.Tests
+{
+    public class ReverseIntegerTests
+    {
+        [Theory]
+        [InlineData(123, 321)]
+        [InlineData(-123, -321)]
+        [InlineData(120, 21)]
+        [InlineData(1, 1)]
+        [InlineData(0, 0)]
+        [InlineData(-1, -1)]
+        [InlineData(-130, -31)]
+        [InlineData(2332, 2332)]
+        public void ReverseTest(int x, int expectedResult)
+        {
+            //Act
+            int result = ReverseIntegerSolution.Reverse(x);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
