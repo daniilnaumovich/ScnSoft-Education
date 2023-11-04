186cf7ea14e7051e715ca2f5bf0826a332a97ff4
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-04 03:59:02 +0300

    Working solution for Palindrom Number problem with Conversion to string approach. Tests refined to use InlineData and Theory instead of Fact test methods.

--- ScnSoft-Education/LeetCodeTraining/PalindromeNumber/PalindromeNumberSolution.cs
+++ ScnSoft-Education/LeetCodeTraining/PalindromeNumber/PalindromeNumberSolution.cs
@@ -1,34 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCodeTraining.PalindromeNumber
+﻿namespace LeetCodeTraining.PalindromeNumber
 {
     //https://leetcode.com/problems/palindrome-number/
     public static class PalindromeNumberSolution
     {
         public static bool IsPalindrome(int x)
         {
-            if (x <= 0 || x % 10 == 0)
+            if (x < 0)
             {
                 return false;
             }
 
-            List<int> orders = new List<int>();
-            int res = 0;
+            string number = x.ToString();
 
-            while (x != 0)
+            if (number.Length == 1)
             {
-                res = x % 10;
-                orders.Add(res);
-                x = x / 10;
+                return true;
             }
 
-            for (int i = 0; i < orders.Count / 2; i++)
+            for (int i = 0; i < number.Length / 2; i++)
             {
-                if (orders[i] != orders[orders.Count - i - 1])
+                if (number[i] != number[number.Length - 1 - i])
                 {
                     return false;
                 }
