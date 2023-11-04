7272e4d927ccd5cd2a3840c3a1eea1865e919ee5
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-04 21:58:27 +0300

    Revert "#7. The solution for Reverse integer problem."
    This reverts commit b55f7c57987b431ec2e913a948b0e4f7cd6c5b77.

--- ScnSoft-Education/LeetCodeTraining/Program.cs
+++ ScnSoft-Education/LeetCodeTraining/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using LeetCodeTraining.ReverseInteger;
-
 Console.WriteLine("Hello, World!");
-
-Console.WriteLine(ReverseIntegerSolution.Reverse(123));
