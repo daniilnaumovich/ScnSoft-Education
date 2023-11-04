094bfd74986964896170bfc0302c9621f441cc0d
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-04 06:58:16 +0300

    Solution for Longest palindromic substring problem. Not very elegant solution, will try the other one later.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/LongestPalindromeTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeTraining.LongestPalindrome;
+
+namespace LeetCodeTraining.Tests
+{
+    public class LongestPalindromeTests
+    {
+        [Theory]
+        [InlineData("babad", new string[] { "bab", "aba"})]
+        [InlineData("cbbd", new string[] { "bb" })]
+        [InlineData("ccc", new string[] { "ccc" })]
+        [InlineData("a", new string[] { "a" })]
+        [InlineData("dd", new string[] { "dd" })]
+        [InlineData("abab", new string[] { "aba", "bab" })]
+        [InlineData("cccc", new string[] { "cccc" })]
+        [InlineData("bcbabdb", new string[] { "bcb", "bab", "bdb" })]
+        public void LongestPalindromeTest(string input, string[] expectedResults)
+        {
+            //Act
+            string result = LongestPalindromeSolution.LongestPalindrome(input);
+
+            //Assert
+            Assert.Contains(expectedResults, x => x == result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/LongestPalindrome/LongestPalindromeSolution.cs
@@ -0,0 +1,63 @@
+﻿namespace LeetCodeTraining.LongestPalindrome
+{
+    //https://leetcode.com/problems/longest-palindromic-substring/
+    public static class LongestPalindromeSolution
+    {
+        public static string LongestPalindrome(string s)
+        {
+            string longestPalindromeSubstring = String.Empty;
+            string currentSub = String.Empty;
+            int startIndex;
+            int endIndex;
+
+            if (s.Length == 1)
+            {
+                return s;
+            }
+
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                for (int j = 0; j <= 1; j++)
+                {
+                    if (j == 0)
+                    {
+                        currentSub = s[i].ToString();
+                        startIndex = i;
+                        endIndex = i;
+                    }
+                    else if (j == 1 && s[i + 1] == s[i])
+                    {
+                        currentSub = s[i].ToString() + s[i + 1].ToString();
+                        startIndex = i;
+                        endIndex = i + 1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    while (startIndex - 1 >= 0 && endIndex + 1 < s.Length)
+                    {
+                        if (s[startIndex - 1] == s[endIndex + 1])
+                        {
+                            currentSub = $"{s[startIndex - 1]}{currentSub}{s[endIndex + 1]}";
+                            startIndex = startIndex - 1;
+                            endIndex = endIndex + 1;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    if (currentSub.Length > longestPalindromeSubstring.Length)
+                    {
+                        longestPalindromeSubstring = currentSub;
+                    }
+                }
+            }
+
+            return longestPalindromeSubstring;
+        }
+    }
+}
