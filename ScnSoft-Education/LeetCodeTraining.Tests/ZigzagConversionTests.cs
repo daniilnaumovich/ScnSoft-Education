47ed3cd09369d9c8884432974d6a6a8d258bafe6
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-04 07:32:44 +0300

    #6. The solution to Zigzag conversion problem

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/ZigzagConversionTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeTraining.ZigzagConversion;
+
+namespace LeetCodeTraining.Tests
+{
+    public class ZigzagConversionTests
+    {
+        [Theory]
+        [InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+        [InlineData("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+        [InlineData("PAYPALISHIRING", 1, "PAYPALISHIRING")]
+        [InlineData("PAYPALISHIRING", 2, "PYAIHRNAPLSIIG")]
+        [InlineData("A", 1, "A")]
+        [InlineData("A", 2, "A")]
+        [InlineData("A", 3, "A")]
+        [InlineData("ABC", 1, "ABC")]
+        [InlineData("ABC", 2, "ACB")]
+        public void FindMedianSortedArraysTest(string input, int numRows, string expectedResult)
+        {
+            //Act
+            string result = ZigzagConversionSolution.Convert(input, numRows);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/ZigzagConversion/ZigzagConversionSolution.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCodeTraining.ZigzagConversion
+{
+    //https://leetcode.com/problems/zigzag-conversion/
+    public static class ZigzagConversionSolution
+    {
+        public static string Convert(string s, int numRows)
+        {
+            char[] result = new char[s.Length];
+
+            int lookupIndex;
+            int counter = 0;
+
+            if (s.Length <= numRows || numRows == 1)
+            {
+                return s;
+            }
+
+            for (int row = 0; row < numRows; row++)
+            {
+                lookupIndex = row;
+
+                while (true)
+                {
+                    result[counter] = s[lookupIndex];
+                    counter++;
+
+                    lookupIndex = lookupIndex + 2 * numRows - 2 * row - 2;
+
+                    if (lookupIndex >= s.Length || counter >= s.Length)
+                    {
+                        break;
+                    }
+
+                    result[counter] = s[lookupIndex];
+
+                    if (lookupIndex % (numRows - 1) != 0)
+                    {
+                        counter++;
+                    }
+
+                    lookupIndex = lookupIndex + 2 * row;
+
+                    if (lookupIndex >= s.Length || counter >= s.Length)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return new String(result);
+        }
+    }
+}
