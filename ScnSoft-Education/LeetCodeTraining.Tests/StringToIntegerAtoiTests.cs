467b2a50ba1f868826950706d8f692127ccd261b
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-05 04:59:29 +0300

    #8. The solution for String to integer Atoi problem.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/StringToIntegerAtoiTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeTraining.StringToIntegerAtoi;
+
+namespace LeetCodeTraining.Tests
+{
+    public class StringToIntegerAtoiTests
+    {
+        [Theory]
+        [InlineData("42", 42)]
+        [InlineData("   -42", -42)]
+        [InlineData("4193 with words", 4193)]
+        [InlineData("words and 222", 0)]
+        [InlineData("onlywords", 0)]
+        [InlineData("-+12", 0)]
+        [InlineData("-2147483648", int.MinValue)]
+        [InlineData("2147483647", int.MaxValue)]
+        [InlineData("0", 0)]
+        [InlineData("2000000000000000", int.MaxValue)]
+        [InlineData("-1111111111111111111", int.MinValue)]
+        public void MyAtoiTest(string s, int expectedResult)
+        {
+            //Act
+            int result = StringToIntegerAtoiSolution.MyAtoi(s);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/StringToIntegerAtoi/StringToIntegerAtoiSolution.cs
@@ -0,0 +1,80 @@
+﻿namespace LeetCodeTraining.StringToIntegerAtoi
+{
+    //https://leetcode.com/problems/string-to-integer-atoi/
+    public static class StringToIntegerAtoiSolution
+    {
+        public static int MyAtoi(string s)
+        {
+            string numberString = String.Empty;
+            long result;
+            int sign = 1;
+
+            int i = 0;
+
+            if (s == String.Empty)
+            {
+                return 0;
+            }
+
+            while (i < s.Length && s[i] == ' ')
+            {
+                i++;
+            }
+
+            if (i < s.Length && s[i] == '-')
+            {
+                sign = -1;
+                i++;
+            }
+            else if (i < s.Length && s[i] == '+')
+            {
+                i++;
+            }
+
+            if (i < s.Length && Char.IsDigit(s[i]))
+            {
+                while (i < s.Length && Char.IsDigit(s[i]))
+                {
+                    numberString += s[i];
+                    i++;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+
+            numberString = numberString.TrimStart('0');
+
+            if (sign > 0)
+            {
+                if (int.MaxValue.ToString().Length < numberString.Length)
+                {
+                    return int.MaxValue;
+                }
+            }
+            if (sign < 0)
+            {
+                if (int.MinValue.ToString().Length < numberString.Length)
+                {
+                    return int.MinValue;
+                }
+            }
+
+            if (long.TryParse(numberString, out result))
+            {
+                if (result * sign > int.MaxValue)
+                {
+                    return int.MaxValue;
+                }
+                if (result * sign < int.MinValue)
+                {
+                    return int.MinValue;
+                }
+                return (int)(result * sign);
+            }
+
+            return 0;
+        }
+    }
+}
