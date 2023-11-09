f43586d3cd3b25d3492e074af535908cf12a6d4d
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-09 20:49:27 +0300

    #11. The solution to Integer to Roman problem.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/IntegerToRomanTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeTraining.IntegerToRoman;
+
+namespace LeetCodeTraining.Tests
+{
+    public class IntegerToRomanTests
+    {
+        [Theory]
+        [InlineData(3, "III")]
+        [InlineData(58, "LVIII")]
+        [InlineData(1994, "MCMXCIV")]
+        [InlineData(3999, "MMMCMXCIX")]
+        [InlineData(1999, "MCMXCIX")]
+        [InlineData(777, "DCCLXXVII")]
+        [InlineData(888, "DCCCLXXXVIII")]
+        [InlineData(1475, "MCDLXXV")]
+        public void IntToRomanTests(int number, string expectedResult)
+        {
+            //Act
+            string result = IntegerToRomanSolution.IntToRoman(number);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
