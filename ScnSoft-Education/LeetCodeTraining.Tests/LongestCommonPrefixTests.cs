4f6c3fe5224f84f4eeca0a41234764bbc64b08aa
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-09 21:08:39 +0300

    #13. The solution for Longest Common Prefix problem.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/LongestCommonPrefixTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeTraining.LongestCommonPrefix;
+
+namespace LeetCodeTraining.Tests
+{
+    public class LongestCommonPrefixTests
+    {
+        [Theory]
+        [InlineData(new string[] { "flower", "flow", "flight" }, "fl")]
+        [InlineData(new string[] { "dog", "racecar", "car" }, "")]
+        [InlineData(new string[] { "abc", "abc", "abc", "abc" }, "abc")]
+        [InlineData(new string[] { "def"}, "def")]
+        [InlineData(new string[] { "aabb", "abb"}, "a")]
+        public void LongestCommonPrefixTest(string[] strs, string expectedResult)
+        {
+            //Act
+            string result = LongestCommonPrefixSolution.LongestCommonPrefix(strs);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/LongestCommonPrefix/LongestCommonPrefixSolution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeTraining.LongestCommonPrefix
+{
+    //https://leetcode.com/problems/longest-common-prefix/
+    public static class LongestCommonPrefixSolution
+    {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            string prefix = string.Empty;
+            string shortestWord = strs.ToList().OrderBy(x => x.Length).First();
+
+            for (int i = 0; i < shortestWord.Length; i++)
+            {
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    if (strs[j][i] != shortestWord[i])
+                    {
+                        return prefix;
+                    }
+                }
+
+                prefix += shortestWord[i];
+            }
+
+            return prefix;
+        }
+    }
+}
