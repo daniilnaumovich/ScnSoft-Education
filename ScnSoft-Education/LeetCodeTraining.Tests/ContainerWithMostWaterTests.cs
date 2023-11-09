95ee7776fff3beedade0b6fb6e1b07df48199cf1
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-09 20:32:08 +0300

    #10. The solution to Container with most water problem.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/ContainerWithMostWaterTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeTraining.ContainerWithMostWater;
+
+namespace LeetCodeTraining.Tests
+{
+    public class ContainerWithMostWaterTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+        [InlineData(new int[] { 1, 1 }, 1)]
+        [InlineData(new int[] { 1, 2 }, 1)]
+        [InlineData(new int[] { 2, 2 }, 2)]
+        [InlineData(new int[] { 0, 0 }, 0)]
+        [InlineData(new int[] { 0, 1 }, 0)]
+        [InlineData(new int[] { 2, 3, 4, 5, 18, 17, 6 }, 17)]
+        [InlineData(new int[] { 1, 2, 2, 0, 0, 0, 1 }, 6)]
+        [InlineData(new int[] { 1, 7, 7, 0, 0, 0, 1 }, 7)]
+        [InlineData(new int[] { 2, 3, 4, 3, 5, 25, 24, 6 }, 24)]
+        public void MaxAreaTests(int[] heights, int expectedResult)
+        {
+            //Act
+            int result = ContainerWithMostWaterSolution.MaxArea(heights);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/ContainerWithMostWater/ContainerWithMostWaterSolution.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCodeTraining.ContainerWithMostWater
+{
+    //https://leetcode.com/problems/container-with-most-water/
+    public static class ContainerWithMostWaterSolution
+    {
+        public static int MaxArea(int[] height)
+        {
+            int leftIndex = 0;
+            int rightIndex = height.Length - 1;
+            int currentVolume;
+
+            int maxVolume = Math.Min(height[0], height[height.Length - 1]) * (height.Length - 1);
+
+            if (height.Length == 2)
+            {
+                return maxVolume;
+            }
+
+            while (true)
+            {
+                while (height[leftIndex] <= height[rightIndex])
+                {
+                    if (leftIndex >= rightIndex)
+                    {
+                        break;
+                    }
+
+                    leftIndex++;
+
+                    currentVolume = Math.Min(height[leftIndex], height[rightIndex]) * (rightIndex - leftIndex);
+                    if (currentVolume > maxVolume)
+                    {
+                        maxVolume = currentVolume;
+                    }
+                }
+
+                while (height[rightIndex] <= height[leftIndex])
+                {
+                    if (rightIndex <= leftIndex)
+                    {
+                        break;
+                    }
+
+                    rightIndex--;
+
+                    currentVolume = Math.Min(height[leftIndex], height[rightIndex]) * (rightIndex - leftIndex);
+                    if (currentVolume > maxVolume)
+                    {
+                        maxVolume = currentVolume;
+                    }
+                }
+
+                if (leftIndex >= rightIndex)
+                {
+                    break;
+                }
+            }
+
+            return maxVolume;
+        }
+    }
+}
