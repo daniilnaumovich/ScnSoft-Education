4d8836514b9f668487e6e28e0740fe5448c16114
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-13 02:18:35 +0300

    #19. The solution for Valid Parentheses problem.

---
+++ ScnSoft-Education/LeetCodeTraining/ValidParentheses/ValidParenthesesSolution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCodeTraining.ValidParentheses
+{
+    //https://leetcode.com/problems/valid-parentheses/
+    public static class ValidParenthesesSolution
+    {
+        public static bool IsValid(string s)
+        {
+            Dictionary<char, char> parentheses = new Dictionary<char, char>()
+            {
+                {'(', ')'},
+                {'{', '}'},
+                {'[', ']'}
+            };
+
+            Stack<char> st = new Stack<char>();
+            char current;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (parentheses.ContainsKey(s[i]))
+                {
+                    st.Push(s[i]);
+                }
+                else if (st.TryPeek(out current) == true && parentheses[st.Peek()] == s[i])
+                {
+                    st.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (st.TryPeek(out current) == true)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
