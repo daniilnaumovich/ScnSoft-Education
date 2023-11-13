dbb645e35749e0a17fd10fd4f96211252b60c1f9
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-13 03:14:42 +0300

    #21. The solution for Generate Parentheses problem.

---
+++ ScnSoft-Education/LeetCodeTraining/GenerateParentheses/GenerateParenthesesSolution.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace LeetCodeTraining.GenerateParentheses
+{
+    //https://leetcode.com/problems/generate-parentheses/
+    public static class GenerateParenthesesSolution
+    {
+        public static IList<string> GenerateParenthesis(int n)
+        {
+            if (n == 1)
+            {
+                return new List<string>() { "()" };
+            }
+
+            StringBuilder sb;
+            List<string> currentList = (List<string>)GenerateParenthesis(n - 1);
+            HashSet<string> result = new HashSet<string>();
+
+            foreach (string s in currentList)
+            {
+                for (int i = 0; i <= s.Length; i++)
+                {
+                    sb = new StringBuilder(s);
+                    result.Add(sb.Insert(i, "()").ToString());
+                }
+            }
+
+            return result.ToList<string>();
+        }
+    }
+}
