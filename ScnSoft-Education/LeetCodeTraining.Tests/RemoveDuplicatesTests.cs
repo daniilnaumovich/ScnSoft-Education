e4a2de80b03e72b72a32c7bc473ef9cf4a397ab0
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-13 03:58:29 +0300

    #25. The solution for Remove Duplicates from Sorted Array problem.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/RemoveDuplicatesTests.cs
@@ -0,0 +1,22 @@
+﻿using LeetCodeTraining.RemoveDuplicates;
+
+namespace LeetCodeTraining.Tests
+{
+    public class RemoveDuplicatesTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 1, 2 }, 2)]
+        [InlineData(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+        [InlineData(new int[] { 1, 2, 3 ,4, 5, 6, 7, 8, 9, 10 }, 10)]
+        [InlineData(new int[] { 1 }, 1)]
+        [InlineData(new int[] { 1, 1, 1, 1, 1, 1 }, 1)]
+        public void RemoveDuplicatesTest(int[] nums, int expectedResult)
+        {
+            //Act
+            int result = RemoveDuplicatesSolution.RemoveDuplicates(nums);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/RemoveDuplicates/RemoveDuplicatesSolution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeTraining.RemoveDuplicates
+{
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+    public static class RemoveDuplicatesSolution
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            HashSet<int> uniqueNums = new HashSet<int>(nums);
+
+            int index = 0;
+            foreach (int num in uniqueNums)
+            {
+                nums[index] = num;
+                index++;
+            }
+
+            return uniqueNums.Count();
+        }
+    }
+}
