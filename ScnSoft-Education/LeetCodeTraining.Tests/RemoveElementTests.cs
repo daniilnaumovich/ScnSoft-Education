ccb3d17a3470747989375cec5ebf159f4fd4f726
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-13 04:21:17 +0300

    #26. The solution for Remove Element problem.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/RemoveElementTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCodeTraining.RemoveElement;
+
+namespace LeetCodeTraining.Tests
+{
+    public class RemoveElementTests
+    {
+        [Theory]
+        [InlineData(new int[] { 3, 2, 2, 3 }, 3, 2)]
+        [InlineData(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2, 5)]
+        [InlineData(new int[] { 1 }, 1, 0)]
+        [InlineData(new int[] { 5 }, 1, 1)]
+        [InlineData(new int[] { 1, 1, 1, 1, 2}, 1, 1)]
+        [InlineData(new int[] { 2, 2, 2 }, 2, 0)]
+        public void RemoveElementTest(int[] nums, int val, int expectedResult)
+        {
+            //Act
+            int result = RemoveElementSolution.RemoveElement(nums, val);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/RemoveElement/RemoveElementSolution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeTraining.RemoveElement
+{
+    //https://leetcode.com/problems/remove-element/
+    public static class RemoveElementSolution
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int currentIndex = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[currentIndex] = nums[i];
+                    currentIndex++;
+                }
+            }
+            return currentIndex;
+        }
+    }
+}
