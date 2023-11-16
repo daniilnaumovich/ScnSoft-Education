a11bf0b79a4f86b8dcfe13bd6ed1261c5e840829
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-17 00:21:55 +0300

    #2. Solution for Longest Substring without duplicate chars problem. (#4)
    * Solution for Longest Substring without duplicate chars problem.
    
    * added link to problem
    
    * Refined the solution to compute length of substring directly instead of getting the substring itself.
    Rewrote tests with InlineData approach.
    
    * Removed unnecessary files.

---
+++ ScnSoft-Education/LeetCodeTraining/LongestSubstring/LongestSubstringSolution.cs
@@ -0,0 +1,48 @@
+﻿namespace LeetCodeTraining.LongestSubstring
+{
+    //https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    public static class LongestSubstringSolution
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+
+            if (s.Length == 1)
+            {
+                return 1;
+            }
+
+            int longestSubstringLength = 0;
+            int currentSubstringLength = 0;
+
+            Dictionary<char, int> usedCharacters = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (!usedCharacters.ContainsKey(s[j]))
+                    {
+                        usedCharacters.Add(s[j], j);
+                        currentSubstringLength += 1;
+                    }
+                    else
+                    {
+                        usedCharacters.Clear();
+                        if (currentSubstringLength > longestSubstringLength)
+                        {
+                            longestSubstringLength = currentSubstringLength;
+                        }
+                        currentSubstringLength = 0;
+                        break;
+                    }
+                }
+            }
+
+            return longestSubstringLength;
+        }
+    }
+}
