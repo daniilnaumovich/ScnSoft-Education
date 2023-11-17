d0db1903ed3a26bac6229f062ed07be8bc5f55b0
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-17 15:51:25 +0300

    #3: Solution for Palindrome Number problem.  (#3)
    * Solution for Palindrome Number problem. Passes all tests on LeetCode except for one which has duplicate test with different result (isPalindrome(0) should be false and true simultaneously)
    
    * Working solution for Palindrom Number problem with Conversion to string approach.
    Tests refined to use InlineData and Theory instead of Fact test methods.
    
    * Removed unnecessary files.
    
    * Added "-121" and "10" test cases

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/PalindromeNumberSolutionTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeTraining.PalindromeNumber;
+
+namespace LeetCodeTraining.Tests
+{
+    public class PalindromeNumberSolutionTests
+    {
+        [Theory]
+        [InlineData(-123, false)]
+        [InlineData(-121, false)]
+        [InlineData(10, false)]
+        [InlineData(0, true)]
+        [InlineData(1, true)]
+        [InlineData(121, true)]
+        [InlineData(123454321, true)]
+        [InlineData(121211, false)]
+        [InlineData(1000, false)]
+        [InlineData(1212121, true)]
+        [InlineData(12122121, true)]
+        public void IsPalindromeTest(int number, bool expectedResult)
+        {
+            //Act
+            var result = PalindromeNumberSolution.IsPalindrome(number);
+
+            //Assert
+            Assert.Equal(result, expectedResult);         
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/PalindromeNumber/PalindromeNumberSolution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeTraining.PalindromeNumber
+{
+    //https://leetcode.com/problems/palindrome-number/
+    public static class PalindromeNumberSolution
+    {
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+
+            string number = x.ToString();
+
+            if (number.Length == 1)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < number.Length / 2; i++)
+            {
+                if (number[i] != number[number.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
