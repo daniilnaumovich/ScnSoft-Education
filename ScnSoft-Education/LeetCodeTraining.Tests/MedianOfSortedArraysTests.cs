da3d3b2b286f5dd35c53d35ae12d96a8372488a4
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-18 09:40:01 +0300

    Refined solution to meet O(log(m+n)) time complexity requirement. Lines in test file were formatted.

--- ScnSoft-Education/LeetCodeTraining.Tests/MedianOfSortedArraysTests.cs
+++ ScnSoft-Education/LeetCodeTraining.Tests/MedianOfSortedArraysTests.cs
@@ -9,8 +9,8 @@ namespace LeetCodeTraining.Tests
         [InlineData(new int[] { 1, 2 }, new int[] { 3, 4 }, 2.5)]
         [InlineData(new int[] { 1, 2 }, new int[] { 1, 1 }, 1)]
         [InlineData(new int[] { 1, 2, 3 }, new int[] { 3, 4 }, 3)]
-        [InlineData(new int[] { 0, 0 }, new int[2] { 0, 1000}, 0)]
-        [InlineData(new int[] { 11, 12 }, new int[2] { 13, 14}, 12.5)]
+        [InlineData(new int[] { 0, 0 }, new int[] { 0, 1000 }, 0)]
+        [InlineData(new int[] { 11, 12 }, new int[] { 13, 14 }, 12.5)]
         public void FindMedianSortedArraysTest(int[] nums1, int[] nums2, double expectedResult)
         {
             //Act
--- ScnSoft-Education/LeetCodeTraining/MedianOfSortedArrays/MedianOfSortedArraysSolution.cs
+++ ScnSoft-Education/LeetCodeTraining/MedianOfSortedArrays/MedianOfSortedArraysSolution.cs
@@ -5,27 +5,74 @@
     {
         public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
         {
-            int[] resultingArray = new int[nums1.Length + nums2.Length];
+            int[] shorter;
+            int[] longer;
 
-            for (int i = 0; i < nums1.Length; i++)
+            if (nums1.Length < nums2.Length)
             {
-                resultingArray[i] = nums1[i];
-            }
-
-            for (int i = nums1.Length; i < resultingArray.Length; i++)
-            {
-                resultingArray[i] = nums2[i - nums1.Length];
-            }
-
-            Array.Sort(resultingArray);
-
-            if (resultingArray.Length % 2 == 1)
-            {
-                return (double)resultingArray[resultingArray.Length / 2];
+                shorter = nums1;
+                longer = nums2;
             }
             else
             {
-                return ((double)resultingArray[resultingArray.Length / 2] + (double)resultingArray[resultingArray.Length / 2 - 1]) / 2;
+                shorter = nums2;
+                longer = nums1;
+            }
+
+            if (shorter.Length == 0)
+            {
+                return longer.Length % 2 == 0
+                    ? (double)(longer[longer.Length / 2 - 1] + longer[longer.Length / 2]) / 2
+                    : longer[longer.Length / 2];
+            }
+
+            if (shorter.Length == 1 && longer.Length == 1)
+            {
+                return (double)(shorter[0] + longer[0]) / 2;
+            }
+
+            int resultIndex = (nums1.Length + nums2.Length + 1) / 2;
+
+            int left = 0;
+            int right = shorter.Length;
+
+            int sr = 0,
+                sl = 0,
+                lr = 0,
+                ll = 0;
+
+            while (left <= right)
+            {
+                int shorterPtr = (right + left) / 2;
+
+                int longerPtr = resultIndex - shorterPtr;
+
+                sr = shorterPtr < shorter.Length ? shorter[shorterPtr] : int.MaxValue;
+                sl = shorterPtr - 1 >= 0 ? shorter[shorterPtr - 1] : int.MinValue;
+                lr = longerPtr < longer.Length ? longer[longerPtr] : int.MaxValue;
+                ll = longerPtr - 1 >= 0 ? longer[longerPtr - 1] : int.MinValue;
+
+                if (sr >= ll && lr >= sl)
+                {
+                    break;
+                }
+                else if (sl > lr)
+                {
+                    right = shorterPtr - 1;
+                }
+                else
+                {
+                    left = shorterPtr + 1;
+                }
+            }
+
+            if ((shorter.Length + longer.Length) % 2 == 0)
+            {
+                return (double)(Math.Max(sl, ll) + Math.Min(sr, lr)) / 2;
+            }
+            else
+            {
+                return Math.Max(sl, ll);
             }
         }
     }
