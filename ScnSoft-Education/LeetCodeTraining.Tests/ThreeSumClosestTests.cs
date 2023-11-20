dc07bdc7c599ed43c46fa536fa1e333bbb974185
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-20 14:04:33 +0300

    #15. The solution for Three Sum Closest Problem

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/ThreeSumClosestTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeTraining._3SumClosest;
+
+namespace LeetCodeTraining.Tests
+{
+    public class ThreeSumClosestTests
+    {
+        [Theory]
+        [InlineData(new int[] { -1, 2, 1, -4 }, 1, 2)]
+        [InlineData(new int[] { 0, 0, 0 }, 1, 0)]
+        [InlineData(new int[] { 1, 2, 4, 10 }, int.MaxValue, 16)]
+        [InlineData(new int[] { -6, -20, 3, 6, -7 }, int.MinValue, -33)]
+        [InlineData(new int[] { 1, 1, 1 }, 1, 3)]
+        [InlineData(new int[] { -4, -2, 0, 2, 4, -3, -1, 1, 3 }, -10, -9)]
+        [InlineData(new int[] { -4, -2, 0, 2, 4, -3, -1, 1, 3 }, 10, 9)]
+        [InlineData(new int[] { -4, -2, 0, 2, 4, -3, -1, 1, 3 }, -5, -5)]
+        [InlineData(new int[] { -4, -2, 0, 2, 4, -3, -1, 1, 3 }, 4, 4)]
+        public void ThreeSumClosestTest(int[] nums, int target, int expectedResult)
+        {
+            //Act
+            int result = ThreeSumClosestSolution.ThreeSumClosest(nums, target);
+
+            //Assert
+            Assert.Equal(result, expectedResult);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/3SumClosest/ThreeSumClosestSolution.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCodeTraining._3SumClosest
+{
+    //https://leetcode.com/problems/3sum-closest/
+    public static class ThreeSumClosestSolution
+    {
+        public static int ThreeSumClosest(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int diff = int.MaxValue;
+            int result = 0;
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+
+                int lp = i + 1;
+                int rp = nums.Length - 1;
+
+                while (lp < rp)
+                {
+                    int sum = nums[i] + nums[lp] + nums[rp];
+
+                    if (sum == target)
+                    {
+                        return sum;
+                    }
+
+                    if (Math.Abs(sum - target) < diff)
+                    {
+                        diff = Math.Abs(sum - target);
+                        result = sum;
+                    }
+
+                    if (sum > target)
+                    {
+                        while (rp >= 1 && nums[rp] == nums[rp - 1])
+                        {
+                            rp--;
+                        }
+                        rp--;
+                        continue;
+                    }
+                    while (lp < nums.Length - 1 && nums[lp] == nums[lp + 1])
+                    {
+                        lp++;
+                    }
+                    lp++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
