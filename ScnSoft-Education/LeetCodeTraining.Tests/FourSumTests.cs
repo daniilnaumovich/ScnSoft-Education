ba852aa5b70452e796d4f70b01dc0fe0bea130ab
daniilnaumovich <dnaumovich@scnsoft.com>
2023-11-20 17:15:26 +0300

    #18. The solution for FourSum problem.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/FourSumTests.cs
@@ -0,0 +1,46 @@
+﻿using LeetCodeTraining.FourSum;
+
+namespace LeetCodeTraining.Tests
+{
+    public class FourSumTests
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void ThreeSumTest(int[] nums, int target, IList<IList<int>> expectedResult)
+        {
+            //Act
+            IList<IList<int>> result = FourSumSolution.FourSum(nums, target);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    //nums
+                    new int[] { 1, 0, -1, 0, -2, 2 }, 
+                    //target
+                    0,      
+                    //expectedResult
+                    new List<IList<int>>()                  
+                    {
+                        new List<int>() { -2, -1, 1, 2 },
+                        new List<int>() { -2, 0, 0, 2 },
+                        new List<int>() { -1, 0, 0, 1 }
+                    }
+                },
+                new object[]
+                {
+                    new int[] { 2, 2, 2, 2, 2 },
+                    8,
+                    new List<IList<int>>() 
+                    {
+                        new List<int>() { 2, 2, 2, 2 }
+                    }
+                }                
+            };
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/FourSum/FourSumSolution.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCodeTraining.FourSum
+{
+    //https://leetcode.com/problems/4sum/
+    public static class FourSumSolution
+    {
+        public static IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            IList<IList<int>> result = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length - 3; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+
+                for (int j = i + 1; j < nums.Length - 2; j++)
+                {
+                    if (j > i + 1 && nums[j] == nums[j - 1])
+                    {
+                        continue;
+                    }
+
+                    int lp = j + 1;
+                    int rp = nums.Length - 1;
+
+                    while (lp < rp)
+                    {
+                        long sum = (long)nums[i] + (long)nums[j] + (long)nums[lp] + (long)nums[rp];
+
+                        if (sum == target)
+                        {
+                            result.Add(new List<int>() { nums[i], nums[j], nums[lp], nums[rp] });
+                        }
+                        else if (sum > target)
+                        {
+                            while (rp >= 1 && nums[rp] == nums[rp - 1])
+                            {
+                                rp--;
+                            }
+                            rp--;
+                            continue;
+                        }
+                        while (lp < nums.Length - 1 && nums[lp] == nums[lp + 1])
+                        {
+                            lp++;
+                        }
+                        lp++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
