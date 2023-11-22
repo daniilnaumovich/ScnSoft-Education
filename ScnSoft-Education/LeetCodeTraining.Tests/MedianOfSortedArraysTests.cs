ff8497078e02645d74fa47cf033b2623d1f9f0fc
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-22 12:28:04 +0300

    #4. The solution for Median of sorted arrays problem (#6)
    * #5. The solution for Median of sorted arrays problem
    
    * Refined solution to meet O(log(m+n)) time complexity requirement. Lines in test file were formatted.
    
    * Removed extra lines and redundant else block

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/MedianOfSortedArraysTests.cs
@@ -0,0 +1,23 @@
+﻿using LeetCodeTraining.MedianOfSortedArrays;
+
+namespace LeetCodeTraining.Tests
+{
+    public class MedianOfSortedArraysTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 3 }, new int[] { 2 }, 2)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 3, 4 }, 2.5)]
+        [InlineData(new int[] { 1, 2 }, new int[] { 1, 1 }, 1)]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 3, 4 }, 3)]
+        [InlineData(new int[] { 0, 0 }, new int[] { 0, 1000 }, 0)]
+        [InlineData(new int[] { 11, 12 }, new int[] { 13, 14 }, 12.5)]
+        public void FindMedianSortedArraysTest(int[] nums1, int[] nums2, double expectedResult)
+        {
+            //Act
+            double result = MedianOfSortedArraysSolution.FindMedianSortedArrays(nums1, nums2);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/MedianOfSortedArrays/MedianOfSortedArraysSolution.cs
@@ -0,0 +1,74 @@
+﻿namespace LeetCodeTraining.MedianOfSortedArrays
+{
+    //https://leetcode.com/problems/median-of-two-sorted-arrays/
+    public static class MedianOfSortedArraysSolution
+    {
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] shorter;
+            int[] longer;
+
+            if (nums1.Length < nums2.Length)
+            {
+                shorter = nums1;
+                longer = nums2;
+            }
+            else
+            {
+                shorter = nums2;
+                longer = nums1;
+            }
+
+            if (shorter.Length == 0)
+            {
+                return longer.Length % 2 == 0
+                    ? (double)(longer[longer.Length / 2 - 1] + longer[longer.Length / 2]) / 2
+                    : longer[longer.Length / 2];
+            }
+
+            if (shorter.Length == 1 && longer.Length == 1)
+            {
+                return (double)(shorter[0] + longer[0]) / 2;
+            }
+
+            int resultIndex = (nums1.Length + nums2.Length + 1) / 2;
+            int left = 0;
+            int right = shorter.Length;
+            int sr = 0,
+                sl = 0,
+                lr = 0,
+                ll = 0;
+
+            while (left <= right)
+            {
+                int shorterPtr = (right + left) / 2;
+                int longerPtr = resultIndex - shorterPtr;
+
+                sr = shorterPtr < shorter.Length ? shorter[shorterPtr] : int.MaxValue;
+                sl = shorterPtr - 1 >= 0 ? shorter[shorterPtr - 1] : int.MinValue;
+                lr = longerPtr < longer.Length ? longer[longerPtr] : int.MaxValue;
+                ll = longerPtr - 1 >= 0 ? longer[longerPtr - 1] : int.MinValue;
+
+                if (sr >= ll && lr >= sl)
+                {
+                    break;
+                }
+                else if (sl > lr)
+                {
+                    right = shorterPtr - 1;
+                }
+
+                left = shorterPtr + 1;
+            }
+
+            if ((shorter.Length + longer.Length) % 2 == 0)
+            {
+                return (double)(Math.Max(sl, ll) + Math.Min(sr, lr)) / 2;
+            }
+            else
+            {
+                return Math.Max(sl, ll);
+            }
+        }
+    }
+}
