94864ff0aedb9076ac38d0b96e79138344511c6f
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-23 14:33:43 +0300

    #6. The solution to Zigzag conversion problem (#7)
    * #6. The solution to Zigzag conversion problem
    
    * Removed duplicated blocks of code and implemented them as nested method.
    Resubmitted solution to LC and updated result screenshot.
    
    * Removed extra lines
    
    * Removed unnecessary using
    
    * Removed unnecessary passed variables to CalculateIndeces nested method
    Applied early return in CalculateIndeces method

---
+++ ScnSoft-Education/LeetCodeTraining/ZigzagConversion/ZigzagConversionSolution.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCodeTraining.ZigzagConversion
+{
+    //https://leetcode.com/problems/zigzag-conversion/
+    public static class ZigzagConversionSolution
+    {
+        public static string Convert(string s, int numRows)
+        {
+            if (s.Length <= numRows || numRows == 1)
+            {
+                return s;
+            }
+
+            char[] result = new char[s.Length];
+            int lookupIndex;
+            int counter = 0;
+            bool fromTop; // responsible for the way of calculating the next char
+
+            for (int row = 0; row < numRows; row++)
+            {
+                lookupIndex = row;
+                fromTop = true;
+
+                while (true)
+                {
+                    result[counter] = s[lookupIndex];
+
+                    CalculateIndeces(row);
+
+                    if (lookupIndex >= s.Length || counter >= s.Length)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return new string(result);
+
+            void CalculateIndeces(int row)
+            {
+                if (fromTop)
+                {
+                    counter++;
+                    lookupIndex = lookupIndex + 2 * numRows - 2 * row - 2;
+                    fromTop = false;
+                    return;
+                }
+
+                if (lookupIndex % (numRows - 1) != 0)
+                {
+                    counter++;
+                }
+
+                lookupIndex = lookupIndex + 2 * row;
+                fromTop = true;
+            }
+        }
+    }
+}
