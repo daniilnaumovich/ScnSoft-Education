d366fbb4cf3069e7a70901a4038935966c299f7b
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-24 12:32:14 +0300

    #5. The solution for Longest palindromic substring problem. (#5)
    * Solution for Longest palindromic substring problem.
    Not very elegant solution, will try the other one later.
    
    * More effective solution, screenshot updated.
    
    * Rewrote the code to get rid of duplicated fragments.
    Resubmitted code to LC and updated result screenshot as well.
    
    * 1. Added Alternative solution with separate method for calculating the new start index and length of longest palindromic substring.
    2. Improved readability and formatted the existing solution.
    3. Added test entry and applied MemberData approach with static Data property for test cases.
    4. LC results' screenshots for both main and alternative solutions were updated.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/LongestPalindromeTests.cs
@@ -0,0 +1,44 @@
+﻿using LeetCodeTraining.LongestPalindrome;
+
+namespace LeetCodeTraining.Tests
+{
+    public class LongestPalindromeTests
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void LongestPalindromeTest(string input, string[] expectedResults)
+        {
+            //Act
+            string result = LongestPalindromeSolution.LongestPalindrome(input);
+
+            //Assert
+            Assert.Contains(expectedResults, x => x == result);
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void LongestPalindromeAlternativeTest(string input, string[] expectedResults)
+        {
+            //Act
+            string result = LongestPalindromeSolution.LongestPalindromeAlternative(input);
+
+            //Assert
+            Assert.Contains(expectedResults, x => x == result);
+        }
+
+        public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[] { "babad", new string[] { "bab", "aba" } },
+            new object[] { "cbbd", new string[] { "bb" } },
+            new object[] { "ccc", new string[] { "ccc" } },
+            new object[] { "a", new string[] { "a" } },
+            new object[] { "dd", new string[] { "dd" } },
+            new object[] { "abab", new string[] { "aba", "bab" } },
+            new object[] { "cccc", new string[] { "cccc" } },
+            new object[] { "babba", new string[] { "abba" } },
+            new object[] { "fabccbad", new string[] { "abccba" } },
+            new object[] { "bcbabdb", new string[] { "bcb", "bab", "bdb" }}
+        };
+    }
+}
