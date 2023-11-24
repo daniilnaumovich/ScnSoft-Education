8a1439155faa2911c12eab0787ff05c5f445316d
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-24 12:34:26 +0300

    #7. The solution for Reverse integer problem. (#9)
    * #7. The solution for Reverse integer problem.
    
    * Updated LC result screenshot
    
    * Introduced bool variable for tracking of result's positivity.
    
    * Updated LC result screenshot for bool-introduced solution.

---
+++ ScnSoft-Education/LeetCodeTraining/ReverseInteger/ReverseIntegerSolution.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace LeetCodeTraining.ReverseInteger
+{
+    //https://leetcode.com/problems/reverse-integer/
+    public static class ReverseIntegerSolution
+    {
+        public static int Reverse(int x)
+        {
+            string positiveString = x.ToString();
+            bool isPositive = true;
+            StringBuilder builder = new StringBuilder();
+
+            if (x < 0)
+            {
+                isPositive = false;
+                positiveString = positiveString.Substring(1);
+            }
+
+            for (int i = 0; i < positiveString.Length; i++)
+            {
+                builder.Append(positiveString[positiveString.Length - 1 - i]);
+            }
+
+            if (!int.TryParse(builder.ToString(), out int result))
+            {
+                return 0;
+            }
+
+            return isPositive ? result : -result;
+        }
+    }
+}
