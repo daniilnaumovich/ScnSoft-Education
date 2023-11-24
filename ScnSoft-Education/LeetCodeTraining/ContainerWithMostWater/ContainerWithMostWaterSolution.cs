da511a909354d687cf9173865a1158e55987177b
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-24 13:34:36 +0300

    #11. The solution to Container with most water problem. (#11)
    * #10. The solution to Container with most water problem.
    
    * Updated LC result screenshot
    
    * Removed duplication where possible by introducing methods GetCurrentVolume() and SetMaxVolume().
    Returns are located inside main cycle.
    Variable currentVolume was moved to SetMaxVolume() method scope.
    Resubmitted solution to LC and updated result screenshot.

---
+++ ScnSoft-Education/LeetCodeTraining/ContainerWithMostWater/ContainerWithMostWaterSolution.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCodeTraining.ContainerWithMostWater
+{
+    //https://leetcode.com/problems/container-with-most-water/
+    public static class ContainerWithMostWaterSolution
+    {
+        public static int MaxArea(int[] height)
+        {
+            int leftIndex = 0;
+            int rightIndex = height.Length - 1;
+            int maxVolume = GetCurrentVolume();
+
+            if (height.Length == 2)
+            {
+                return maxVolume;
+            }
+
+            while (true)
+            {
+                while (height[leftIndex] <= height[rightIndex])
+                {
+                    if (rightIndex <= leftIndex++)
+                    {
+                        return maxVolume;
+                    }
+
+                    SetMaxVolume();
+                }
+
+                while (height[rightIndex] <= height[leftIndex])
+                {
+                    if (rightIndex-- <= leftIndex)
+                    {
+                        return maxVolume;
+                    }
+
+                    SetMaxVolume();
+                }
+            }
+
+            int GetCurrentVolume()
+            {
+                return Math.Min(height[leftIndex], height[rightIndex]) * (rightIndex - leftIndex);
+            }
+
+            void SetMaxVolume()
+            {
+                int currentVolume = GetCurrentVolume();
+
+                if (currentVolume > maxVolume)
+                {
+                    maxVolume = currentVolume;
+                }
+            }
+        }
+    }
+}
