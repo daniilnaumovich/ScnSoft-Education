eae1c1ed27f07e65bb9f4002f8a37b1f43ef9e3b
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-27 12:09:29 +0300

    #13. The solution for Roman to Integer problem. (#13)
    * #12. The solution for Roman to Integer problem.
    
    * Minor changes (extra line, earlier return)
    
    * Applied subtract and assign operators.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/RomanToIntegerTests.cs
@@ -0,0 +1,24 @@
+﻿using LeetCodeTraining.RomanToInteger;
+
+namespace LeetCodeTraining.Tests
+{
+    public class RomanToIntegerTests
+    {
+        [Theory]
+        [InlineData("III", 3)]
+        [InlineData("LVIII", 58)]
+        [InlineData("MCMXCIV", 1994)]
+        [InlineData("MMMCMXCIX", 3999)]
+        [InlineData("IV", 4)]
+        [InlineData("MLXXVI", 1076)]
+        [InlineData("MCCXXXIV", 1234)]
+        public void RomanToIntTests(string input, int expectedResult)
+        {
+            //Act
+            int result = RomanToIntegerSolution.RomanToInt(input);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
