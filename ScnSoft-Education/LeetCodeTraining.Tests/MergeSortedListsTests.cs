71a9a6dc13f55f38edfaf6fbe246ab8bf16cc4df
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-29 13:03:50 +0300

    #21. The solution for Merge Two Sorted Lists problem. (#22)
    * #20. The solution for Merge Two Sorted Lists problem.
    
    * Improved solution by removing duplicate code.
    LC result screenshot was updated.
    
    * Added line for vertical alignment

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/MergeSortedListsTests.cs
@@ -0,0 +1,109 @@
+﻿using LeetCodeTraining.MergeSortedLists;
+using System.Text;
+
+namespace LeetCodeTraining.Tests
+{
+    public class MergeSortedListsTests
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void MergeTwoListsTests(int[] list1, int[] list2, string expectedResult)
+        {
+            //Arrange
+            ListNode l1 = InitializeNode(list1);
+            ListNode l2 = InitializeNode(list2);
+
+            //Act
+            ListNode result = MergeSortedListsSolution.MergeTwoLists(l1, l2);
+            string resultString = DisplayNode(result);
+
+            //Assert
+            Assert.Equal(expectedResult, resultString);
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new int[] { 1, 2, 4 },
+                    new int[] { 1, 3, 4 },
+                    "112344"
+                },
+                new object[]
+                {
+                    new int[] { },
+                    new int[] { },
+                    string.Empty
+                },
+                new object[]
+                {
+                    new int[] { },
+                    new int[] { 0 },
+                    "0"
+                },
+                new object[]
+                {
+                    new int[] { 1, 1 },
+                    new int[] { 1, 1 },
+                    "1111"
+                },
+                new object[]
+                {
+                    new int[] { 1, 2, 3 },
+                    new int[] { 4, 5, 6 },
+                    "123456"
+                },
+                new object[]
+                {
+                    new int[] { 2, 3, 4 },
+                    new int[] { 1, 5 },
+                    "12345"
+                }
+            };
+
+        public ListNode InitializeNode(int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return null;
+            }
+
+            ListNode h = new ListNode(numbers[0]);
+
+            if (numbers.Length > 1)
+            {
+                ListNode n = new ListNode(numbers[1]);
+                h.next = n;
+                for (int i = 2; i < numbers.Length; i++)
+                {
+                    n.next = new ListNode(numbers[i]);
+                    n = n.next;
+                }
+            }
+
+            return h;
+        }
+
+        public string DisplayNode(ListNode node)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            while (node != null)
+            {
+                sb.Append(node.val);
+
+                if (node.next != null)
+                {
+                    node = node.next;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/MergeSortedLists/MergeSortedListsSolution.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCodeTraining.MergeSortedLists
+{
+    //https://leetcode.com/problems/merge-two-sorted-lists/
+    public static class MergeSortedListsSolution
+    {
+        public static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode start = new ListNode();
+            ListNode head = start;
+
+            while (list1 != null && list2 != null)
+            {
+                if (list1.val > list2.val)
+                {
+                    list2 = AddNode(list2);
+                }
+                else
+                {
+                    list1 = AddNode(list1);
+                }
+            }
+
+            while (list1 != null)
+            {
+                list1 = AddNode(list1);
+            }
+
+            while (list2 != null)
+            {
+                list2 = AddNode(list2);
+            }
+
+            return head.next;
+
+            ListNode AddNode(ListNode node)
+            {
+                start.next = node;
+                start = start.next;
+                return node.next;
+            }
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
