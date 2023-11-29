13cb4d76312a9b2bc7e2b310003f20f931df55e2
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-29 08:01:39 +0300

    #14. The solution for Longest Common Prefix problem. (#14)
    * #13. The solution for Longest Common Prefix problem.
    
    * Switched from concatenation to StringBuilder.
    Minor format corrections.
    
    * Removed ToList() method invocation.
    Rebuilt test method with MemberData attribute, in order to be able to use string.Empty property in test cases.
    LC result screenshot was updated.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/LongestCommonPrefixTests.cs
@@ -0,0 +1,48 @@
+﻿using LeetCodeTraining.LongestCommonPrefix;
+
+namespace LeetCodeTraining.Tests
+{
+    public class LongestCommonPrefixTests
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void LongestCommonPrefixTest(string[] strs, string expectedResult)
+        {
+            //Act
+            string result = LongestCommonPrefixSolution.LongestCommonPrefix(strs);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new string[] { "flower", "flow", "flight" },
+                    "fl"
+                },
+                new object[]
+                {
+                    new string[] { "dog", "racecar", "car" },
+                    string.Empty
+                },
+                new object[]
+                {
+                    new string[] { "abc", "abc", "abc", "abc" },
+                    "abc"
+                },
+                new object[]
+                {
+                    new string[] { "def" },
+                    "def"
+                },
+                new object[]
+                {
+                    new string[] { "aabb", "abb" },
+                    "a"
+                }
+            };
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/LongestCommonPrefix/LongestCommonPrefixSolution.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace LeetCodeTraining.LongestCommonPrefix
+{
+    //https://leetcode.com/problems/longest-common-prefix/
+    public static class LongestCommonPrefixSolution
+    {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            StringBuilder prefix = new StringBuilder();
+            string shortestWord = strs.OrderBy(x => x.Length).First();
+
+            for (int i = 0; i < shortestWord.Length; i++)
+            {
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    if (strs[j][i] != shortestWord[i])
+                    {
+                        return prefix.ToString();
+                    }
+                }
+
+                prefix.Append(shortestWord[i]);
+            }
+
+            return prefix.ToString();
+        }
+    }
+}
