42fa7bbf871a2fc8789be5b212ac598f83460ce2
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-29 12:41:59 +0300

    #19. The solution for Remove Nth Node From End of List problem. (#21)
    * #18. The solution for Remove Nth Node From End of List problem.
    
    * Introduced recursive solution for "Remove Nth Node from end" problem in RemoveNthFromEndRec() method in addition to existing solution in RemoveNthFromEnd() method.
    Minor formatting changes.
    Created test method for recursive solution and rebuilt them to use MemberData attribute instead of InlineData.
    LC results' screenshots for both solutions were updated.
    
    * Removed else block

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/RemoveNthNodeTests.cs
@@ -0,0 +1,123 @@
+﻿using LeetCodeTraining.RemoveNthNode;
+using System.Text;
+
+namespace LeetCodeTraining.Tests
+{
+    public class RemoveNthNodeTests
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void RemoveNthNodeTest(int[] list, int number, string expectedResult)
+        {
+            //Arrange
+            ListNode head = InitializeNode(list);
+
+            //Act
+            ListNode result = RemoveNthNodeSolution.RemoveNthFromEnd(head, number);
+            string resultString = DisplayNode(result);
+
+            //Assert
+            Assert.Equal(expectedResult, resultString);
+        }
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void RemoveNthNodeRecTest(int[] list, int number, string expectedResult)
+        {
+            //Arrange
+            ListNode head = InitializeNode(list);
+
+            //Act
+            ListNode result = RemoveNthNodeSolution.RemoveNthFromEndRec(head, number);
+            string resultString = DisplayNode(result);
+
+            //Assert
+            Assert.Equal(expectedResult, resultString);
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new int[] { 1, 2, 3, 4, 5 },
+                    2,
+                    "1235"
+                },
+                new object[]
+                {
+                    new int[] { 1, 2, 3, 4, 5, 6, 7 },
+                    3,
+                    "123467"
+                },
+                new object[]
+                {
+                    new int[] { 1, 2, 3, 4, 5 },
+                    5,
+                    "2345"
+                },
+                new object[]
+                {
+                    new int[] { 1, 2, 3, 4, 5 },
+                    1,
+                    "1234"
+                },
+                new object[]
+                {
+                    new int[] { 1 },
+                    1,
+                    string.Empty
+                },
+                new object[]
+                {
+                    new int[] { 1, 2 },
+                    1,
+                    "1"
+                }
+            };
+
+        private ListNode InitializeNode(int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return null;
+            }
+
+            ListNode h = new ListNode(numbers[0]);
+
+            if (numbers.Length > 1)
+            {
+                ListNode n = new ListNode(numbers[1]);
+                h.next = n;
+
+                for (int i = 2; i < numbers.Length; i++)
+                {
+                    n.next = new ListNode(numbers[i]);
+                    n = n.next;
+                }
+            }
+
+            return h;
+        }
+
+        private string DisplayNode(ListNode node)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            while (node != null)
+            {
+                sb.Append(node.val);
+
+                if (node.next != null)
+                {
+                    node = node.next;
+                    continue;
+                }
+
+                break;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/RemoveNthNode/RemoveNthNodeSolution.cs
@@ -0,0 +1,93 @@
+﻿namespace LeetCodeTraining.RemoveNthNode
+{
+    //https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+    public static class RemoveNthNodeSolution
+    {
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (head.next == null)
+            {
+                return null;
+            }
+
+            ListNode first = new ListNode();
+            first.next = head;
+            ListNode tail = first;
+            ListNode start = tail.next.next;
+            ListNode end = start;
+
+            for (int i = 0; i < n - 2; i++)
+            {
+                end = end.next;
+            }
+
+            while (true)
+            {
+                if (end.next != null)
+                {
+                    end = end.next;
+                    start = start.next;
+                    tail = tail.next;
+                }
+                else
+                {
+                    if (n == 1)
+                    {
+                        tail.next.next = null;
+                    }
+                    else
+                    {
+                        tail.next = start;
+                    }
+
+                    break;
+                }
+            }
+
+            return first.next;
+        }
+
+        public static ListNode RemoveNthFromEndRec(ListNode head, int n)
+        {
+            ListNode first = new ListNode();
+            first.next = head;
+            int distance = 0;
+
+            CalculateDistance(first);
+
+            return first.next;
+
+            void CalculateDistance(ListNode head)
+            {
+                if (head.next == null)
+                {
+                    distance = 1;
+                    return;
+                }
+
+                CalculateDistance(head.next);
+
+                if (distance == n)
+                {
+                    head.next = head.next.next;
+                }
+
+                if (distance > 0)
+                {
+                    distance++;
+                }
+            }
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
