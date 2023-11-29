5d67e4b646b2423da729e724d9475d67c70656c6
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-29 08:02:18 +0300

    #16. The solution for Three Sum Closest Problem (#25)
    * #15. The solution for Three Sum Closest Problem
    
    * Changed maxDiff value to 13000 instead of int.MaxValue
    Introduced currentDiff variable and renamed diff to maxDiff
    Fixed formatting (added lines)

---
+++ ScnSoft-Education/LeetCodeTraining/3SumClosest/ThreeSumClosestSolution.cs
@@ -0,0 +1,63 @@
+﻿namespace LeetCodeTraining._3SumClosest
+{
+    //https://leetcode.com/problems/3sum-closest/
+    public static class ThreeSumClosestSolution
+    {
+        public static int ThreeSumClosest(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int maxDiff = 13000;
+            int result = 0;
+            int lp;
+            int rp;
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+
+                lp = i + 1;
+                rp = nums.Length - 1;
+
+                while (lp < rp)
+                {
+                    int sum = nums[i] + nums[lp] + nums[rp];
+                    int currentDiff = Math.Abs(sum - target);
+
+                    if (sum == target)
+                    {
+                        return sum;
+                    }
+
+                    if (currentDiff <= maxDiff)
+                    {
+                        maxDiff = currentDiff;
+                        result = sum;
+                    }
+
+                    if (sum > target)
+                    {
+                        while (rp > 2 && nums[rp] == nums[rp - 1])
+                        {
+                            rp--;
+                        }
+                        
+                        rp--;
+                        continue;
+                    }
+
+                    while (lp < nums.Length - 1 && nums[lp] == nums[lp + 1])
+                    {
+                        lp++;
+                    }
+
+                    lp++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
