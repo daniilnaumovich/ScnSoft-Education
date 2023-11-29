ee8bb9be9a81df54c2d16058251afb3080352c5e
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-29 08:01:54 +0300

    #17. The solution for Letter Combinations of a Phone Number problem. (#15)
    * #16. The solution for Letter Combinations of a Phone Number problem.
    
    * Fixed LC result screenshot
    
    * Rearranged solution, removed unnecessary checks and redundant blocks.
    Resubmitted code to LC and updated result screenshot.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/PhoneNumberCombinationsTests.cs
@@ -0,0 +1,39 @@
+﻿using LeetCodeTraining.PhoneNumberCombinations;
+
+namespace LeetCodeTraining.Tests
+{
+    public class PhoneNumberCombinationsTests
+    {
+        [Theory]
+        [InlineData("23", new string[] { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" } )]
+        [InlineData("2", new string[] { "a", "b", "c" })]
+        [InlineData("", new string[] { })]
+        [InlineData("576", new string[] 
+        {
+            "jpm", "jpn", "jpo",
+            "jqm", "jqn", "jqo",
+            "jrm", "jrn", "jro",
+            "jsm", "jsn", "jso",
+            "kpm", "kpn", "kpo",
+            "kqm", "kqn", "kqo",
+            "krm", "krn", "kro",
+            "ksm", "ksn", "kso",
+            "lpm", "lpn", "lpo",
+            "lqm", "lqn", "lqo",
+            "lrm", "lrn", "lro",
+            "lsm", "lsn", "lso"
+        })]
+        public void LetterCombinationsTests(string digits, string[] expectedResult)
+        {
+            //Arrange
+            Array.Sort(expectedResult);
+
+            //Act
+            string[] result = PhoneNumberCombinationsSolution.LetterCombinations(digits).ToArray();
+            Array.Sort(result);       
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/PhoneNumberCombinations/PhoneNumberCombinationsSolution.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace LeetCodeTraining.PhoneNumberCombinations
+{
+    //https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+    public static class PhoneNumberCombinationsSolution
+    {
+        public static IList<string> LetterCombinations(string digits)
+        {
+            if (digits.Length == 0)
+            {
+                return new List<string>();
+            }
+
+            List<string> output = new List<string>();
+            Dictionary<char, string> numberToChar = new Dictionary<char, string>()
+            {
+                {'2', "abc"},
+                {'3', "def"},
+                {'4', "ghi"},
+                {'5', "jkl"},
+                {'6', "mno"},
+                {'7', "pqrs"},
+                {'8', "tuv"},
+                {'9', "wxyz"}
+            };
+
+            foreach (char c in numberToChar[digits[^1]])
+            {
+                output.Add(c.ToString());
+            }
+
+            if (digits.Length == 1)
+            {
+                return output;
+            }
+
+            for (int i = digits.Length - 2; i >= 0; i--)
+            {
+                AppendLettersOfDigit(numberToChar[digits[i]]);
+            }
+
+            return output;
+
+            void AppendLettersOfDigit(string chars)
+            {
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = output.Count - 1; i >= 0; i--)
+                {
+                    for (int j = 0; j < chars.Length - 1; j++)
+                    {
+                        output.Insert(i, output[i]);
+                    }
+                }
+
+                for (int k = 0; k < output.Count; k++)
+                {
+                    builder.Append(output[k]);
+                    builder.Insert(0, chars[k % chars.Length]);
+                    output[k] = builder.ToString();
+                    builder.Clear();
+                }
+            }
+        }
+    }
+}
