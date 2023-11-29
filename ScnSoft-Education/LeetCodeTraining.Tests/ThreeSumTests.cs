5409f6fcb7ebbb901d652b5f30d7146dd90653cc
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-29 08:02:06 +0300

    #15. The solution for 3Sum problem. (#24)
    * #14. The solution for 3Sum problem.
    
    * Minor formatting changes.
    LC result screenshot updated.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/ThreeSumTests.cs
@@ -0,0 +1,56 @@
+﻿using LeetCodeTraining.ThreeSum;
+
+namespace LeetCodeTraining.Tests
+{
+    public class ThreeSumTests
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void ThreeSumTest(int[] nums, IList<IList<int>> expectedResult)
+        {
+            //Act
+            IList<IList<int>> result = ThreeSumSolution.ThreeSum(nums);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new int[] { -1, 0, 1, 2, -1, -4 },
+                    new List<IList<int>>()
+                    {
+                        new List<int>() { -1, -1, 2 },
+                        new List<int>() { -1, 0, 1 }
+                    }
+                },
+                new object[]
+                {
+                    new int[] { 0, 1, 1 },
+                    new List<IList<int>>() { }
+                },
+                new object[]
+                {
+                    new int[] { 0, 0, 0 },
+                    new List<IList<int>>()
+                    {
+                        new List<int>() { 0, 0, 0 }
+                    }
+                },
+                new object[]
+                {
+                    new int[] { 0, 1, 2, -2, -1, 3, 0, 0 },
+                    new List<IList<int>>()
+                    {
+                        new List<int>() { -2, -1, 3 },
+                        new List<int>() { -2, 0, 2 },
+                        new List<int>() { -1, 0, 1 },
+                        new List<int>() { 0, 0, 0 }
+                    }
+                }
+            };
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/ThreeSum/ThreeSumSolution.cs
@@ -0,0 +1,53 @@
+﻿namespace LeetCodeTraining.ThreeSum
+{
+    //https://leetcode.com/problems/3sum/
+    public static class ThreeSumSolution
+    {
+        public static IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums);
+            IList<IList<int>> result = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1])
+                {
+                    continue;
+                }
+
+                int lp = i + 1;
+                int rp = nums.Length - 1;
+                int target = -nums[i];
+
+                while (lp < rp)
+                {
+                    int sum = nums[lp] + nums[rp];
+
+                    if (sum == target)
+                    {
+                        result.Add(new List<int>() { nums[i], nums[lp], nums[rp] });
+                    }
+                    else if (sum > target)
+                    {
+                        while (rp > 2 && nums[rp] == nums[rp - 1])
+                        {
+                            rp--;
+                        }
+
+                        rp--;
+                        continue;
+                    }
+
+                    while (lp < nums.Length - 1 && nums[lp] == nums[lp + 1])
+                    {
+                        lp++;
+                    }
+
+                    lp++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
