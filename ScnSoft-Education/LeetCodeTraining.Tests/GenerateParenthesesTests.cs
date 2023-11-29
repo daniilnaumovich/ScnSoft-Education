5c504225c9f64d1f8f9896425b4d046ba18940b5
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-29 13:04:12 +0300

    #22. The solution for Generate Parentheses problem. (#18)
    * #21. The solution for Generate Parentheses problem.
    
    * Corrected formatting in test class.
    
    * Removed unnecessary creation of StringBuilder instance for every iteration of a cycle.
    Updated LC result screenshot.

---
+++ ScnSoft-Education/LeetCodeTraining.Tests/GenerateParenthesesTests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeTraining.GenerateParentheses;
+
+namespace LeetCodeTraining.Tests
+{
+    public class GenerateParenthesesTests
+    {
+        [Theory]
+        [InlineData(1, new string[] { "()" })]
+        [InlineData(2, new string[] { "()()", "(())" })]
+        [InlineData(3, new string[] { "((()))", "(()())", "(())()", "()(())", "()()()" })]
+        [InlineData(4, new string[] { "()()()()", "(())()()", "()(())()", "()()(())",
+                                      "(()())()", "((()))()", "()(()())", "()((()))",
+                                      "(())(())", "(()()())", "((())())", "(()(()))",
+                                      "((()()))", "(((())))" })]
+        public void GenerateParenthesisTests(int n, string[] expectedResult)
+        {
+            //Arrange
+            Array.Sort(expectedResult);
+
+            //Act
+            string[] result = GenerateParenthesesSolution.GenerateParenthesis(n).ToArray();
+            Array.Sort(result);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ ScnSoft-Education/LeetCodeTraining/GenerateParentheses/GenerateParenthesesSolution.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace LeetCodeTraining.GenerateParentheses
+{
+    //https://leetcode.com/problems/generate-parentheses/
+    public static class GenerateParenthesesSolution
+    {
+        public static IList<string> GenerateParenthesis(int n)
+        {
+            if (n == 1)
+            {
+                return new List<string>() { "()" };
+            }
+
+            StringBuilder sb = new StringBuilder();
+            List<string> currentList = (List<string>)GenerateParenthesis(n - 1);
+            HashSet<string> result = new HashSet<string>();
+
+            foreach (string s in currentList)
+            {
+                for (int i = 0; i <= s.Length; i++)
+                {
+                    sb.Append(s);
+                    result.Add(sb.Insert(i, "()").ToString());
+                    sb.Clear();
+                }
+            }
+
+            return result.ToList();
+        }
+    }
+}
