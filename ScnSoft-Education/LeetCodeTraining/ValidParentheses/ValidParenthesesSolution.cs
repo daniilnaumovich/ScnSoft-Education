97e3cbc7a9c07920164b02f08e1713536b5bfe95
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-30 23:00:10 +0300

    #20. The solution for Valid Parentheses problem. (#17)
    * #19. The solution for Valid Parentheses problem.
    
    * Rearranged and formatted the solution a little bit.
    LC result screenshot was updated.
    
    * Removed last else block by adding continue keyword to if blocks, and changed TryPeek() methods to Any().
    
    * Used TryPop() instead of checking with Any() and Pop()'ing the element afterwards.

---
+++ ScnSoft-Education/LeetCodeTraining/ValidParentheses/ValidParenthesesSolution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeTraining.ValidParentheses
+{
+    //https://leetcode.com/problems/valid-parentheses/
+    public static class ValidParenthesesSolution
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<char> st = new Stack<char>();
+            Dictionary<char, char> parentheses = new Dictionary<char, char>()
+            {
+                {'(', ')'},
+                {'{', '}'},
+                {'[', ']'}
+            };
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (parentheses.ContainsKey(s[i]))
+                {
+                    st.Push(s[i]);
+                    continue;
+                }
+
+                if (st.TryPop(out char p) && parentheses[p] == s[i])
+                {
+                    continue;
+                }
+                
+                return false;
+            }
+
+            if (st.Any())
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
