b86afcd449d8d12f6bb6430e61a7f927652bd8be
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-11-30 23:17:12 +0300

    #28. The solution for Find the Index of the First Occurrence in a String problem. Implemented without inbuilt IndexOf() method. (#38)

---
+++ ScnSoft-Education/LeetCodeTraining/IndexOfSubstring/IndexOfSubstringSolution.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeTraining.IndexOfSubstring
+{
+    //https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+    public static class IndexOfSubstringSolution
+    {
+        public static int StrStr(string haystack, string needle)
+        {
+            bool isResult;
+
+            for (int i = 0; i < haystack.Length - needle.Length + 1; i++)
+            {
+                isResult = true;
+
+                for (int j = 0; j < needle.Length; j++)
+                {
+                    if (needle[j] != haystack[i + j])
+                    {
+                        isResult = false;
+                        break;
+                    }
+                }
+
+                if (isResult)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
