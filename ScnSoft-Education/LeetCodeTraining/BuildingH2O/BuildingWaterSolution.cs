533a5ed2d61f86e35de4ea15122bbce89cb36499
daniilnaumovich <dnaumovich@scnsoft.com>
2023-12-02 02:22:55 +0300

    Only H2O class is the solution itself - all of the rest methods/classes serve testing purposes. Did not put them into test class because they try to imitate the way of how the program actually works, even though the way wasn't explicitly described on LeetCode.

---
+++ ScnSoft-Education/LeetCodeTraining/BuildingH2O/BuildingWaterSolution.cs
@@ -0,0 +1,108 @@
+﻿using System.Text;
+
+public static class BuildingWaterSolution
+{
+    public static string BuildWaterMolecules(string input)
+    {
+        StringBuilder molecules = new StringBuilder();
+        H2O water = new H2O();
+        Action releaseH = () => { molecules.Append("H"); };
+        Action releaseO = () => { molecules.Append("O"); };
+        Task[] tasks = new Task[input.Length];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            switch (input[i])
+            {
+                case 'H':
+                    HydrogenGenerator genH = new HydrogenGenerator(water, releaseH);
+                    Task h = Task.Factory.StartNew(genH.GenerateElement);
+                    tasks[i] = h;
+                    break;
+                case 'O':
+                    OxygenGenerator genO = new OxygenGenerator(water, releaseO);
+                    Task o = Task.Factory.StartNew(genO.GenerateElement);
+                    tasks[i] = o;
+                    break;
+            }
+        }
+
+        Task.WaitAll(tasks);
+
+        return molecules.ToString();
+    }
+}
+
+public class H2O
+{
+    SemaphoreSlim hydrogen = new SemaphoreSlim(0, 2);
+    SemaphoreSlim oxygen = new SemaphoreSlim(1, 1);
+    object locker = new object();
+    int hydrogenCounter = 0;
+
+    public H2O() { }
+
+    public void Hydrogen(Action releaseHydrogen)
+    {
+        hydrogen.Wait();
+
+        lock (locker)
+        {
+            // releaseHydrogen() outputs "H". Do not change or remove this line.
+            releaseHydrogen();
+            hydrogenCounter++;
+        }
+
+        if (hydrogenCounter == 2)
+        {
+            oxygen.Release();
+            hydrogenCounter = 0;
+        }
+    }
+
+    public void Oxygen(Action releaseOxygen)
+    {
+        oxygen.Wait();
+        // releaseOxygen() outputs "O". Do not change or remove this line.
+        releaseOxygen();
+        hydrogen.Release(2);
+    }
+}
+
+public class HydrogenGenerator
+{
+    private readonly Random rnd = new Random();
+    private readonly H2O water;
+    private readonly Action generator;
+
+    public HydrogenGenerator(H2O h2o, Action generateElement)
+    {
+        water = h2o;
+        generator = generateElement;
+    }
+
+    public void GenerateElement()
+    {
+        Task.Delay(rnd.Next(500));
+        water.Hydrogen(generator);
+    }
+}
+
+public class OxygenGenerator
+{
+    private Random rnd = new Random();
+    private H2O water;
+    private Action generator;
+
+    public OxygenGenerator(H2O h2o, Action generateElement)
+    {
+        water = h2o;
+        generator = generateElement;
+    }
+
+    public void GenerateElement()
+    {
+        Task.Delay(rnd.Next(500));
+        water.Oxygen(generator);
+    }
+}
