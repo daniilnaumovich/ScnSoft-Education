2b3aa1b5f2d1aceddd4efdcbf9ed0429bbb565aa
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-12-05 18:48:56 +0300

    #26. The solution for Remove Duplicates from Sorted Array problem. (#19)
    * #25. The solution for Remove Duplicates from Sorted Array problem.
    
    * Solution using in-place swapping instead of HashSet approach.
    LC result screenshot updated.
    
    * Minor changes:
    1) Moved increment operator inside indexer;
    2) Set if condition inside loop to != instead of > as input array is sorted.
    
    * Added test methods for checking the order of sorted elements and for presence of duplicates.

---
+++ ScnSoft-Education/LeetCodeTraining/RemoveDuplicates/RemoveDuplicatesSolution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeTraining.RemoveDuplicates
+{
+    //https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+    public static class RemoveDuplicatesSolution
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int index = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    nums[index++] = nums[i];
+                }
+            }
+
+            return index;
+        }
+    }
+}
