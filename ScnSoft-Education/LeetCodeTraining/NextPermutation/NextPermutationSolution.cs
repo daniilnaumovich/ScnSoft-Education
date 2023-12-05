713eab2a640c3d3978a0d49122968f3968df6c94
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-12-05 04:44:29 +0300

    #31. The solution for Next Permutation problem. (#33)
    * #31. The solution for Next Permutation problem.
    
    * Formatted lines
    
    * Removed call to inbuilt Array.Sort() method and replaced it with inplace Swap().
    
    * Removede unused usings

---
+++ ScnSoft-Education/LeetCodeTraining/NextPermutation/NextPermutationSolution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCodeTraining.NextPermutation
+{
+    //https://leetcode.com/problems/next-permutation/
+    public static class NextPermutationSolution
+    {
+        public static void NextPermutation(int[] nums)
+        {
+            int revertIndex = -1;
+            int swapIndex = -1;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i + 1] > nums[i])
+                {
+                    revertIndex = i;
+                }
+
+                if (revertIndex != -1 && nums[i + 1] > nums[revertIndex])
+                {
+                    swapIndex = i + 1;
+                }
+            }
+
+            if (revertIndex != -1 && swapIndex != -1)
+            {
+                Swap(ref nums[revertIndex], ref nums[swapIndex]);
+            }
+
+            for (int i = 0; i < (nums.Length - revertIndex - 1) / 2; i++)
+            {
+                Swap(ref nums[revertIndex + i + 1], ref nums[nums.Length - i - 1]);
+            }
+
+            void Swap(ref int first, ref int second)
+            {
+                int swap = first;
+                first = second;
+                second = swap;
+            }
+        }
+    }
+}
