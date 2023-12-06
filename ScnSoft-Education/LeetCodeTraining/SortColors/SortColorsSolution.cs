de014f9efb441540d42c7a73da2d4e14a0398bb9
daniilnaumovich <149603422+daniilnaumovich@users.noreply.github.com>
2023-12-06 15:43:57 +0300

    #75. The solution for Sort Colors problem. (#40)
    * #75. The solution for Sort Colors problem.
    
    * Added vertical alignment
    Switched from rptr (Red pointer) and bptr (Blue pointer) to lp and rp as left and red pointer respectively

---
+++ ScnSoft-Education/LeetCodeTraining/SortColors/SortColorsSolution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeTraining.SortColors
+{
+    //https://leetcode.com/problems/sort-colors/
+    public static class SortColorsSolution
+    {
+        public static void SortColors(int[] nums)
+        {
+            int lp = 0;
+            int rp = nums.Length;
+
+            for (int i = 0; i < rp; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    Swap(lp, i);
+                    lp++;
+                    continue;
+                }
+
+                if (nums[i] == 2)
+                {
+                    Swap(i, rp - 1);
+                    rp--;
+
+                    if (nums[i] != 1)
+                    {
+                        i--;
+                    }
+                }
+            }
+
+            void Swap(int firstIndex, int secondIndex)
+            {
+                int number = nums[firstIndex];
+                nums[firstIndex] = nums[secondIndex];
+                nums[secondIndex] = number;
+            }
+        }
+    }
+}
